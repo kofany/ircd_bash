392cbaa1db12250ceaa4cd41aa5266ab86789873
kofany <92736603+kofany@users.noreply.github.com>
2021-10-22 15:04:00 +0200

    Fail-safe options added.

--- tor.sh
+++ tor.sh
@@ -22,7 +22,7 @@ chk_last=$(cat ${conf_dir}/timestamp |grep "${timestamp}")
 get_tor()
 {
 #if tor dir del all files inside else make dir 
-[[ -d "${conf_dir}/tor" ]] && rm -rf ${conf_dir}/tor/* || mkdir ${conf_dir}/tor/
+[[ -d "${conf_dir}/tor" ]] && { mv ${conf_dir}/tor ${conf_dir}/tor_backup; mkdir ${conf_dir}/tor/;} || mkdir ${conf_dir}/tor/
 links=("https://lists.fissionrelays.net/tor/exits.txt" "https://www.dan.me.uk/torlist/?exit")
 for link in ${links[@]}; do
 i=$((i+1))
@@ -32,19 +32,23 @@ done
 #gen new target file with uniqe k-lines for ips merged from 2 sources
 gen_kline()
 {
-#del old merge file if exists
-[[ -f "${conf_dir}/tor/merge" ]] && rm -rf ${conf_dir}/tor/merge
+[[ -f "${conf_dir}/tor/merge" ]] && mv ${conf_dir}/tor/merge ${conf_dir}/tor/merge.backup
 exfiles=$(ls ${conf_dir}/tor/)
 for exfile in ${exfiles[@]}; do
 cat ${conf_dir}/tor/${exfile} >> ${conf_dir}/tor/merge
 done
-#remove old target file
-[[ -f "${conf_dir}/${target}" ]] && rm -rf ${conf_dir}/${target}
+#back up old target file
+[[ -f "${conf_dir}/${target}" ]] && mv ${conf_dir}/${target} ${conf_dir}/${target}.backup
 #eliminate duplicated ips with awk
 k_lines=$(awk '!seen[$0]++' ${conf_dir}/tor/merge)
 IFS=$'\n'       # make newlines the only separator
 for k_line in ${k_lines[@]}; do
+if [[ ${k_line} =~ ^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]]; then
 echo -e "K|${k_line}|Tor connections are NOT welcome|*|0|" >> ${conf_dir}/${target}
+fi
+if [[ ${k_line} =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$ ]]; then
+	echo -e "K|${k_line}|Tor connections are NOT welcome|*|0|" >> ${conf_dir}/${target}
+fi
 done
 #timestamp remove old and make new
 [[ -f "${conf_dir}/timestamp" ]] && rm -rf ${conf_dir}/timestamp
@@ -53,6 +57,11 @@ echo -e "${timestamp}" >> ${conf_dir}/timestamp
 }
 reload_ircd()
 {
+	#last fail-safe if target is empty use backup file
+chk_target=$(wc -l < ${conf_dir}/${target})
+if [[ ${chk_target} -lt 10 ]]; then
+	mv ${conf_dir}/${target}.backup ${conf_dir}/${target}
+fi
 ircdpid=$(pgrep ircd)
 timestamp=$(date '+%d/%m/%Y_%H')
 #check if include line with target is in ircd.conf
